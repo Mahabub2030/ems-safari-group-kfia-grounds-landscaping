5e5fa914e07fc5fc86e30830fcba361eb80aee27
Mahabub2030 <mahabubalom72091@gmail.com>
2026-08-01 08:48:58 +0300

    added the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <p className="text-2xl bg-red-200">this ems tailwindcss setup done</p>
+      <p className="text-5xl bg-green-200">this ems tailwindcss setup done</p>
     </div>
   );
 }
