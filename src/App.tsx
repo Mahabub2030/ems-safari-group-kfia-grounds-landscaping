11125686da755b76fcf6bb970f2951fccd206ad8
Mahabub2030 <mahabubalom72091@gmail.com>
2026-08-01 08:44:53 +0300

    juts start

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>this ems from srech</h1>
+      <p className="text-2xl bg-red-200">this ems tailwindcss setup done</p>
+    </div>
+  );
+}
+
+export default App;
